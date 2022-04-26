36847b33e1a76fd56c5d3dad56651f1b8e53cffa
jaimeToca <jaime.toca.munoz@gmail.com>
2022-04-26 12:55:09 +0200

    Screenshot testing

--- app/src/androidTest/java/org/dhis2/Dhis2Runner.java
+++ app/src/androidTest/java/org/dhis2/Dhis2Runner.java
@@ -4,13 +4,11 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
-import androidx.test.runner.AndroidJUnitRunner;
-
+import com.karumi.shot.ShotTestRunner;
 import com.squareup.rx2.idler.Rx2Idler;
-
 import io.reactivex.plugins.RxJavaPlugins;
 
-public class Dhis2Runner extends AndroidJUnitRunner {
+public class Dhis2Runner extends ShotTestRunner {
 
     @Override
     public void onCreate(Bundle arguments) {
